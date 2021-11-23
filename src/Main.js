cd34128b277a782e8c3894bf5c2876887a6316cd
Tim <tchang@gmail.com>
2021-11-22 21:41:34 -1000

    progress

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import './css/App.css';
 import React, { Component } from 'react';
 import Table from './components/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios'
 
 class Main extends Component {
@@ -17,13 +18,17 @@ class Main extends Component {
 
   retrieveData() {
     let pageNumber = 1
+    let reqLink = "https://swapi.dev/api/planets/?page=1"
     while (pageNumber <= 6) {
-      axios.get(`https://swapi.dev/api//planets/?page=${pageNumber}`)
+      axios.get(`https://swapi.dev/api/planets/?page=${pageNumber}`)
         .then((resp) =>  {
           let planets = resp.data.results
           this.setState({
             planetData: [...this.state.planetData, ...planets]
           })
+          console.log(resp.data.next)
+          reqLink = resp.data.next
+          console.log(reqLink)
         })
         .catch((err) => {
           console.log("Error: ", err)
@@ -32,13 +37,31 @@ class Main extends Component {
     }
   }
 
+  processPages() {
+    let tableElements = []
+    let i = 0
+    while (i < this.state.planetData.length) {
+      // 0-9,10-19
+      let dataSet = this.state.planetData.slice(i, i + 10)
+      tableElements.push(<Table planetData = {dataSet} />)
+    }
+    return tableElements
+  }
+
+
   render() {
+    let tableElements = this.processPages()
+    console.log(tableElements)
     return (
       <div className="main-container">
-        <Table planetData = {this.state.planetData}/>
+        {tableElements}
       </div>
     )
   }
 }
 
 export default Main;
+
+// How to Paginate:
+// Divide data up by into sets of 10, pass a set of 10 elements to Table element
+// Add pagination (BS) element and add onclicks to point to appropriate page
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
+import BootStrapTable from 'react-bootstrap/Table';
+import axios from 'axios'
 
 class Table extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      planetData: this.props.planetData
+      planetData: []
     }
   }
 
   componentDidMount() {
+    // this.retrieveData()
+  }
 
+  retrieveData() {
+    let pageNumber = 1
+    while (pageNumber <= 6) {
+      axios.get(`https://swapi.dev/api//planets/?page=${pageNumber}`)
+        .then((resp) =>  {
+          let planets = resp.data.results
+          this.setState({
+            planetData: [...this.state.planetData, ...planets]
+          })
+        })
+        .catch((err) => {
+          console.log("Error: ", err)
+        })
+      pageNumber++;
+    }
   }
 
   render() {
+    console.log(this.props.planetData, "test")
     return (
       <div className = "table-container">
         <div>
-          {this.state.planetData.map((planet) =>
-            <div>
-              Name: {planet.name}
-            </div>
-          )}
+          <BootStrapTable responsive>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Population</th>
+                <th>Rotation Period</th>
+                <th>Orbital Period</th>
+                <th>Diameter</th>
+                <th>Climate</th>
+                <th>Surface Water</th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.props.planetData.map((planet) => (
+              <tr>
+                <td>{planet.name}</td>
+                <td>{planet.population}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.surface_water}</td>
+             </tr>
+           ))}
+            </tbody>
+          </BootStrapTable>
         </div>
       </div>
     )
@@ -29,3 +70,13 @@ class Table extends Component {
 }
 
 export default Table;
+
+// {Array.from({ length: 12 }).map((_, index) => (
+//   <td key={index}>Table cell {index}</td>
+// ))}
+
+// {this.state.planetData.map((planet) =>
+//   <div>
+//     Name: {planet.name}
+//   </div>
+// )}
